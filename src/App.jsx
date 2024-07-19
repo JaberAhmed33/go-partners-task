50ac379a65b1e212678e754424a572984eb8be86
Jaber Ahmed <70724851+JaberAhmed33@users.noreply.github.com>
2024-07-19 20:25:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <main className="bg-main-200 h-[calc(100vh-95px)] w-full lg:w-[75%] px-2 sm:p-4 flex flex-col">
     <CardList />
 
-    <section className="w-full sm:px-8 flex justify-between items-center sm:flex-col sm:items-start my-4">
+    <section className="w-full sm:px-8 flex justify-between items-center sm:flex-col sm:items-start my-2 sm:my-4">
       <h5 className="text-md sm:text-xl lg:text-2xl xl:text-3xl font-semibold text-slate-100 mb-2 md:mb-4">Withdrawals</h5>
       <button className="bg-primary text-sm md:text-md tracking-wides px-2 py-1 t md:px-3 md:py-2 rounded-lg text-slate-100 flex items-center gap-2"><AiOutlinePlus />  Add Withdrawals</button>
     </section>
